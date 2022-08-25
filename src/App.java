dd221b7f5c32ff706790cb178ce4ed75ebf01100
will19742 <william-177@hotmail.com>
2022-08-25 12:02:18 -0600

    Git inicial

---
+++ src/App.java
@@ -0,0 +1,32 @@
+/* Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+Es posible que el codigo generado no sea completamente correcto. Si encuentra
+errores por favor reportelos en el foro (http://pseint.sourceforge.net). */
+
+// En java, el nombre de un archivo fuente debe coincidir con el nombre de la clase que contiene,
+// por lo que este archivo deber�a llamarse "CIRCUITO_GERMICIDA.java."
+
+import java.io.*;
+import java.math.*;
+
+public class App {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));
+		double dist;
+		double dosis;
+		double poten;
+		double tamano_lamp;
+		System.out.println("Determina la dosis de tiempo requerida para desinfectar tu objeto ");
+		System.out.println(" ingresa la medida de la lampara en pulgadas ");
+		tamano_lamp = Double.parseDouble(bufEntrada.readLine());
+		System.out.println(" ingresa la distancia entre la lampara y la superficie a desinfectar ");
+		dist = Double.parseDouble(bufEntrada.readLine());
+		System.out.println(" ingresa la potencia emite la lampara ");
+		poten = Double.parseDouble(bufEntrada.readLine());
+		dosis = ((241)*tamano_lamp*3.1416*(Math.pow(dist,2)))/poten;
+		System.out.println(" la dosis de tiempo requerida para desinfectar el objeto es de "+dosis+" segundos ");
+	}
+
+
+}
+
